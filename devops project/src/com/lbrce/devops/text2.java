4bb54654f2ac1c4eb97ea4eb420edf6e1527dd34
AdmiN <AdmiN@ITWS14>
2023-08-10 15:28:06 +0530

    hi devops additional data

---
+++ devops project/src/com/lbrce/devops/text2.java
@@ -0,0 +1,10 @@
+package com.lbrce.devops;
+
+public class text2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("DEVOPS ADDITIONAL DATA");
+	}
+
+}
